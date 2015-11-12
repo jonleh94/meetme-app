587f83d11655df96f1e387212b30fcf82262d9ce
Jonas Leh <jonleh.94@gmail.com>
2015-11-12 10:57:30 +0100

    Added new JSON Reader - readLocation (2nd push)

--- app/src/main/java/com/example/jansenmo/meetmeapp/ImportTester2.java
+++
@@ -1,7 +0,0 @@
-package com.example.jansenmo.meetmeapp;
-
-/**
- * Created by lehj on 29.10.2015.
- */
-public class ImportTester2 {
-}
--- app/src/main/java/com/example/jansenmo/meetmeapp/Tester.java
+++
@@ -1,8 +0,0 @@
-package com.example.jansenmo.meetmeapp;
-
-/**
- * Created by lehj on 28.10.2015.
- */
-public class Tester {
-
-}
